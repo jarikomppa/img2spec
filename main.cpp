d0dc160308f8fb004935c06d324adbc81b1c8fae
jarikomppa <jari.komppa@gmail.com>
2015-12-05 19:17:33 +0200

    added support for custom cell sizes (8x4, 8x2, 8x1)

--- main.cpp
+++ main.cpp
@@ -90,7 +90,7 @@ unsigned int gBitmapBitm[256 * 192];
 float gBitmapProcFloat[256 * 192 * 3];
 
 // Spectrum format data
-unsigned char gSpectrumAttributes[32 * 24];
+unsigned char gSpectrumAttributes[32 * 24 * 8]; // big enough for 8x1 attribs
 unsigned char gSpectrumBitmap[32 * 192];
 
 // Histogram arrays
@@ -623,18 +623,40 @@ void spectrumize_image()
 		gBitmapSpec[i] = rgb_to_speccy_pal(gBitmapProc[i], 0, 16);
 	}
 
-	for (y = 0; y < 24; y++)
+	int ymax;
+	int cellht;
+	switch (gOptCellSize)
+	{
+	case 0:
+		ymax = 24;
+		cellht = 8;
+		break;
+	case 1:
+		ymax = 48;
+		cellht = 4;
+		break;
+	case 2:
+		ymax = 96;
+		cellht = 2;
+		break;
+	case 3:
+		ymax = 192;
+		cellht = 1;
+		break;
+	}
+
+	for (y = 0; y < ymax; y++)
 	{
 		for (x = 0; x < 32; x++)
 		{
 			// Count bright pixels in cell
 			int brights = 0;
 			int blacks = 0;
-			for (i = 0; i < 8; i++)
+			for (i = 0; i < cellht; i++)
 			{
 				for (j = 0; j < 8; j++)
 				{
-					int loc = (y * 8 + i) * 256 + x * 8 + j;
+					int loc = (y * cellht + i) * 256 + x * 8 + j;
 					if (gBitmapSpec[loc] > 7)
 						brights++;
 					if (gBitmapSpec[loc] == 0)
@@ -648,13 +670,13 @@ void spectrumize_image()
 				brights = 0;
 				break;
 			case 1: // prefer dark
-				brights = (brights >= (8 * 8 - blacks)) * 8;
+				brights = (brights >= (8 * cellht - blacks)) * 8;
 				break;
 			case 2: // default
-				brights = (brights >= (8 * 8 - blacks) / 2) * 8;
+				brights = (brights >= (8 * cellht - blacks) / 2) * 8;
 				break;
 			case 3: // prefer bright
-				brights = (brights >= (8 * 8 - blacks) / 4) * 8;
+				brights = (brights >= (8 * cellht - blacks) / 4) * 8;
 				break;
 			case 4: // only bright
 				brights = 8;
@@ -666,11 +688,11 @@ void spectrumize_image()
 				counts[i] = 0;
 
 			// Remap with just bright OR dark colors, based on whether the whole cell is bright or not
-			for (i = 0; i < 8; i++)
+			for (i = 0; i < cellht; i++)
 			{
 				for (j = 0; j < 8; j++)
 				{
-					int loc = (y * 8 + i) * 256 + x * 8 + j;
+					int loc = (y * cellht + i) * 256 + x * 8 + j;
 					int r = rgb_to_speccy_pal(gBitmapProc[loc], brights, 8);
 					gBitmapSpec[loc] = r;
 					counts[r]++;
@@ -695,7 +717,10 @@ void spectrumize_image()
 			{
 				col1 = (gOptPaper - 1) + brights;
 			}
-			counts[col1] = 0;
+
+			// reset count of selected color to zero so we don't pick it twice
+			counts[col1] = 0; 
+			
 			best = 0;
 			for (i = 0; i < 16; i++)
 			{
@@ -710,11 +735,11 @@ void spectrumize_image()
 			if (col2 == 0 && col1 > 7) col2 = 8;
 
 			// Final pass on cell, select which of two colors we can use
-			for (i = 0; i < 8; i++)
+			for (i = 0; i < cellht; i++)
 			{
 				for (j = 0; j < 8; j++)
 				{
-					int loc = (y * 8 + i) * 256 + x * 8 + j;					
+					int loc = (y * cellht + i) * 256 + x * 8 + j;					
 					gBitmapSpec[loc] = pick_from_2_speccy_cols(gBitmapProc[loc], col1, col2);
 				}
 			}		
@@ -894,12 +919,14 @@ void saveraw()
 
 	FILE * f = NULL;
 
+	int attrib_size_multiplier = 1 << gOptCellSize;
+
 	if (GetSaveFileNameA(&ofn))
 	{
 		grab_speccy_bitmap();
 		FILE * f = fopen(szFileName, "wb");
 		fwrite(gSpectrumBitmap, 32 * 192, 1, f);
-		fwrite(gSpectrumAttributes, 32 * 24, 1, f);
+		fwrite(gSpectrumAttributes, 32 * 24 * attrib_size_multiplier, 1, f);
 		fclose(f);
 	}
 }
@@ -927,6 +954,8 @@ void saveh()
 
 	FILE * f = NULL;
 
+	int attrib_size_multiplier = 1 << gOptCellSize;
+
 	if (GetSaveFileNameA(&ofn))
 	{
 		grab_speccy_bitmap();
@@ -943,7 +972,7 @@ void saveh()
 		}
 		fprintf(f,"\n\n");
 		w = 0;
-		for (i = 0; i < 32 * 24; i++)
+		for (i = 0; i < 32 * 24 * attrib_size_multiplier; i++)
 		{
 			w += fprintf(f, "%3d,", gSpectrumAttributes[i]);
 			if (w > 75)
@@ -979,6 +1008,8 @@ void saveinc()
 
 	FILE * f = NULL;
 
+	int attrib_size_multiplier = 1 << gOptCellSize;
+
 	if (GetSaveFileNameA(&ofn))
 	{
 		grab_speccy_bitmap();
@@ -989,7 +1020,7 @@ void saveinc()
 			fprintf(f, "\t.db #0x%02x\n", gSpectrumBitmap[i]);
 		}
 		fprintf(f, "\n\n");
-		for (i = 0; i < 32 * 24; i++)
+		for (i = 0; i < 32 * 24 * attrib_size_multiplier; i++)
 		{
 			fprintf(f, "\t.db #0x%02x\n", gSpectrumAttributes[i]);
 		}
@@ -1000,16 +1031,33 @@ void saveinc()
 
 void gen_attr_bitm()
 {
+	int cellht;
+	switch (gOptCellSize)
+	{
+	case 0:
+		cellht = 8;
+		break;
+	case 1:
+		cellht = 4;
+		break;
+	case 2:
+		cellht = 2;
+		break;
+	case 3:
+		cellht = 1;
+		break;
+	}
+
 	grab_speccy_bitmap();
 	int i, j;
 	for (i = 0; i < 192; i++)
 	{
 		for (j = 0; j < 256; j++)
 		{
-			int attr = gSpectrumAttributes[(i / 8) * 32 + j / 8];
+			int attr = gSpectrumAttributes[(i / cellht) * 32 + j / 8];
 			int fg = gSpeccyPalette[((attr >> 0) & 7) | (((attr & 64)) >> 3)] | 0xff000000;
 			int bg = gSpeccyPalette[((attr >> 3) & 7) | (((attr & 64)) >> 3)] | 0xff000000;
-			gBitmapAttr[i * 256 + j] = (j % 8 < i % 8) ? fg : bg;			
+			gBitmapAttr[i * 256 + j] = (j % 8 < (((8-cellht)/2) + i % cellht)) ? fg : bg;
 			gBitmapBitm[i * 256 + j] = (gSpectrumBitmap[SPEC_Y(i) * 32 + j / 8] & (1 << (7-(j % 8)))) ? 0xffc0c0c0 : 0xff000000;
 		}
 	}
@@ -1146,7 +1194,7 @@ int main(int, char**)
 				ImGui::Combo("Attribute order", &gOptAttribOrder, "Make bitmap pretty\0Make bitmap compressable\0");
 				ImGui::Combo("Bright attributes", &gOptBright, "Only dark\0Prefer dark\0Normal\0Prefer bright\0Only bright\0");
 				ImGui::Combo("Paper attribute", &gOptPaper, "Optimal\0Black\0Blue\0Red\0Purple\0Green\0Cyan\0Yellow\0White\0");
-				ImGui::Combo("Attribute cell size", &gOptCellSize, "8x8 (standard)\0"); // 8x4 (bicolor)\08x2\08x1\0");
+				ImGui::Combo("Attribute cell size", &gOptCellSize, "8x8 (standard)\08x4 (bicolor)\08x2\08x1\0");
 				ImGui::Combo("Dithering", &gOptDither, "None\0");// Ordered\0Random\0Floyd - Steinberg\0");
 			}
 			ImGui::End();
