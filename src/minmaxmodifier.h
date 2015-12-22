d57a1b67c47b6bd598f19f11dd0b383a8b76820d
jarikomppa <jari.komppa@gmail.com>
2015-12-22 19:32:53 +0200

    min/max modifier separate kernel width/height

--- src/minmaxmodifier.h
+++ src/minmaxmodifier.h
@@ -2,7 +2,7 @@ class MinmaxModifier : public Modifier
 {
 public:
 	float mV;
-	int mArea;
+	int mAreaX, mAreaY;
 	bool mR_en, mG_en, mB_en;
 	bool mMin;
 	int mOnce;
@@ -10,7 +10,8 @@ public:
 	virtual void serialize(FILE * f)
 	{
 		write(f, mV);
-		write(f, mArea);
+		write(f, mAreaX);
+		write(f, mAreaY);
 		write(f, mR_en);
 		write(f, mG_en);
 		write(f, mB_en);
@@ -20,7 +21,8 @@ public:
 	virtual void deserialize(FILE * f)
 	{
 		read(f, mV);
-		read(f, mArea);
+		read(f, mAreaX);
+		read(f, mAreaY);
 		read(f, mR_en);
 		read(f, mG_en);
 		read(f, mB_en);
@@ -37,7 +39,8 @@ public:
 		mMin = true;
 		mV = 1;
 		mOnce = 0;
-		mArea = 3;
+		mAreaX = 2;
+		mAreaY = 2;
 		mR_en = true;
 		mG_en = true;
 		mB_en = true;
@@ -61,14 +64,18 @@ public:
 			if (ImGui::Button("Reset##strength   ")) { gDirty = 1; mV = 1; } ImGui::SameLine();
 			ImGui::Text("Strength");
 
-			if (ImGui::SliderInt("##kernel area", &mArea, 2, 16)) { gDirty = 1; }	ImGui::SameLine();
-			if (ImGui::Button("Reset##kernel area   ")) { gDirty = 1; mArea = 2; } ImGui::SameLine();
-			ImGui::Text("Kernel area");
-			
+			if (ImGui::SliderInt("##kernel areaX", &mAreaX, 1, 20)) { gDirty = 1; }	ImGui::SameLine();
+			if (ImGui::Button("Reset##kernel areaX   ")) { gDirty = 1; mAreaX = 2; } ImGui::SameLine();
+			ImGui::Text("Kernel width");
+
+			if (ImGui::SliderInt("##kernel areaY", &mAreaY, 1, 20)) { gDirty = 1; }	ImGui::SameLine();
+			if (ImGui::Button("Reset##kernel areaY   ")) { gDirty = 1; mAreaY = 2; } ImGui::SameLine();
+			ImGui::Text("Kernel height");
+
 			if (ImGui::Checkbox("Red enable", &mR_en)) { gDirty = 1; } ImGui::SameLine();
 			if (ImGui::Checkbox("Green enable", &mG_en)) { gDirty = 1; } ImGui::SameLine();
 			if (ImGui::Checkbox("Blue enable", &mB_en)) { gDirty = 1; } ImGui::SameLine();
-			if (ImGui::Checkbox("Min (vs max)", &mMin)) { gDirty = 1; }
+			if (ImGui::Checkbox(mMin?"Min###minmax":"Max###minmax", &mMin)) { gDirty = 1; }
 		}
 		ImGui::PopID();
 		return ret;
@@ -90,18 +97,18 @@ public:
 					float rmin = 1;
 					float gmin = 1;
 					float bmin = 1;
-					for (x = 0; x < mArea; x++)
+					for (x = 0; x < mAreaX; x++)
 					{
-						for (y = 0; y < mArea; y++)
+						for (y = 0; y < mAreaY; y++)
 						{
-							if ((j + x - mArea / 2) > 0 &&
-								(j + x - mArea / 2) < 256 &&
-								(i + y - mArea / 2) > 0 &&
-								(i + y - mArea / 2) < 192)
+							if ((j + x - mAreaX / 2) > 0 &&
+								(j + x - mAreaX / 2) < 256 &&
+								(i + y - mAreaY / 2) > 0 &&
+								(i + y - mAreaY / 2) < 192)
 							{
-								float b = buf[((i + y - mArea / 2) * 256 + j + x - mArea / 2) * 3 + 0];
-								float g = buf[((i + y - mArea / 2) * 256 + j + x - mArea / 2) * 3 + 1];
-								float r = buf[((i + y - mArea / 2) * 256 + j + x - mArea / 2) * 3 + 2];
+								float b = buf[((i + y - mAreaY / 2) * 256 + j + x - mAreaX / 2) * 3 + 0];
+								float g = buf[((i + y - mAreaY / 2) * 256 + j + x - mAreaX / 2) * 3 + 1];
+								float r = buf[((i + y - mAreaY / 2) * 256 + j + x - mAreaX / 2) * 3 + 2];
 								if (b < bmin) bmin = b;
 								if (g < gmin) gmin = g;
 								if (r < rmin) rmin = r;
@@ -124,18 +131,18 @@ public:
 					float rmax = 0;
 					float gmax = 0;
 					float bmax = 0;
-					for (x = 0; x < mArea; x++)
+					for (x = 0; x < mAreaX; x++)
 					{
-						for (y = 0; y < mArea; y++)
+						for (y = 0; y < mAreaY; y++)
 						{
-							if ((j + x - mArea / 2) > 0 &&
-								(j + x - mArea / 2) < 256 &&
-								(i + y - mArea / 2) > 0 &&
-								(i + y - mArea / 2) < 192)
+							if ((j + x - mAreaX / 2) > 0 &&
+								(j + x - mAreaX / 2) < 256 &&
+								(i + y - mAreaY / 2) > 0 &&
+								(i + y - mAreaY / 2) < 192)
 							{
-								float b = buf[((i + y - mArea / 2) * 256 + j + x - mArea / 2) * 3 + 0];
-								float g = buf[((i + y - mArea / 2) * 256 + j + x - mArea / 2) * 3 + 1];
-								float r = buf[((i + y - mArea / 2) * 256 + j + x - mArea / 2) * 3 + 2];
+								float b = buf[((i + y - mAreaY / 2) * 256 + j + x - mAreaX / 2) * 3 + 0];
+								float g = buf[((i + y - mAreaY / 2) * 256 + j + x - mAreaX / 2) * 3 + 1];
+								float r = buf[((i + y - mAreaY / 2) * 256 + j + x - mAreaX / 2) * 3 + 2];
 								if (b > bmax) bmax = b;
 								if (g > gmax) gmax = g;
 								if (r > rmax) rmax = r;
