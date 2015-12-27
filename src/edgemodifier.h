09b6ab3f5db807b8a7cfe6685127954ffed73631
jarikomppa <jari.komppa@gmail.com>
2015-12-27 14:09:03 +0200

    added fill color to edge modifier

--- src/edgemodifier.h
+++ src/edgemodifier.h
@@ -6,6 +6,7 @@ public:
 	bool mR_en, mG_en, mB_en;
 	bool mDirectional;
 	bool mSeparate;
+	float mFillColor[3];
 	float mDirection;
 	int mOnce;
 
@@ -19,6 +20,9 @@ public:
 		write(f, mDirectional);
 		write(f, mSeparate);
 		write(f, mDirection);
+		write(f, mFillColor[0]);
+		write(f, mFillColor[1]);
+		write(f, mFillColor[2]);
 	}
 
 	virtual void deserialize(FILE * f)
@@ -31,6 +35,9 @@ public:
 		read(f, mDirectional);
 		read(f, mSeparate);
 		read(f, mDirection);
+		read(f, mFillColor[0]);
+		read(f, mFillColor[1]);
+		read(f, mFillColor[2]);
 	}
 
 	virtual int gettype()
@@ -47,6 +54,9 @@ public:
 		mR_en = mG_en = mB_en = true;
 		mSeparate = false;
 		mDirectional = false;
+		mFillColor[0] = 0;
+		mFillColor[1] = 0;
+		mFillColor[2] = 0;
 		mDirection = 0;
 	}
 
@@ -76,6 +86,10 @@ public:
 			if (ImGui::SliderFloat("##Direction  ", &mDirection, 0, 1)) { gDirty = 1; } ImGui::SameLine();
 			if (ImGui::Button("Reset##Direction   ")) { gDirty = 1; mDirection = 0; } ImGui::SameLine();
 			ImGui::Text("Direction");
+
+			if (ImGui::ColorEdit3("##fill", mFillColor)) { gDirty = 1; } ImGui::SameLine();
+			if (ImGui::Button("Reset##fill   ")) { gDirty = 1; mFillColor[0] = 0; mFillColor[1] = 0; mFillColor[2] = 0; } ImGui::SameLine();
+			ImGui::Text("Fill color");
 
 			if (ImGui::Checkbox("Separate color", &mSeparate)) { gDirty = 1; }ImGui::SameLine();
 			if (ImGui::Checkbox("Directional", &mDirectional)) { gDirty = 1; } ImGui::SameLine();
@@ -158,18 +172,18 @@ public:
 
 			for (i = 0; i < 256 * 192; i++)
 			{
-				if (mB_en && (xedge[i * 3 + 0] * xdir + yedge[i * 3 + 0] * ydir) > mThreshold) gBitmapProcFloat[i * 3 + 0] *= 1.0f - mV;
-				if (mG_en && (xedge[i * 3 + 1] * xdir + yedge[i * 3 + 1] * ydir) > mThreshold) gBitmapProcFloat[i * 3 + 1] *= 1.0f - mV;
-				if (mR_en && (xedge[i * 3 + 2] * xdir + yedge[i * 3 + 2] * ydir) > mThreshold) gBitmapProcFloat[i * 3 + 2] *= 1.0f - mV;
+				if (mB_en && (xedge[i * 3 + 0] * xdir + yedge[i * 3 + 0] * ydir) > mThreshold) gBitmapProcFloat[i * 3 + 0] += (mFillColor[0] - gBitmapProcFloat[i * 3 + 0]) * mV;
+				if (mG_en && (xedge[i * 3 + 1] * xdir + yedge[i * 3 + 1] * ydir) > mThreshold) gBitmapProcFloat[i * 3 + 1] += (mFillColor[1] - gBitmapProcFloat[i * 3 + 1]) * mV;
+				if (mR_en && (xedge[i * 3 + 2] * xdir + yedge[i * 3 + 2] * ydir) > mThreshold) gBitmapProcFloat[i * 3 + 2] += (mFillColor[2] - gBitmapProcFloat[i * 3 + 2]) * mV;
 			}
 		}
 		else
 		{
 			for (i = 0; i < 256 * 192; i++)
 			{
-				if (mB_en && (abs(xedge[i * 3 + 0]) > mThreshold || abs(yedge[i * 3 + 0]) > mThreshold)) gBitmapProcFloat[i * 3 + 0] *= 1.0f - mV;
-				if (mG_en && (abs(xedge[i * 3 + 1]) > mThreshold || abs(yedge[i * 3 + 1]) > mThreshold)) gBitmapProcFloat[i * 3 + 1] *= 1.0f - mV;
-				if (mR_en && (abs(xedge[i * 3 + 2]) > mThreshold || abs(yedge[i * 3 + 2]) > mThreshold)) gBitmapProcFloat[i * 3 + 2] *= 1.0f - mV;
+				if (mB_en && (abs(xedge[i * 3 + 0]) > mThreshold || abs(yedge[i * 3 + 0]) > mThreshold)) gBitmapProcFloat[i * 3 + 0] += (mFillColor[0] - gBitmapProcFloat[i * 3 + 0]) * mV;
+				if (mG_en && (abs(xedge[i * 3 + 1]) > mThreshold || abs(yedge[i * 3 + 1]) > mThreshold)) gBitmapProcFloat[i * 3 + 1] += (mFillColor[1] - gBitmapProcFloat[i * 3 + 1]) * mV;
+				if (mR_en && (abs(xedge[i * 3 + 2]) > mThreshold || abs(yedge[i * 3 + 2]) > mThreshold)) gBitmapProcFloat[i * 3 + 2] += (mFillColor[2] - gBitmapProcFloat[i * 3 + 2]) * mV;
 			}
 		}
 
