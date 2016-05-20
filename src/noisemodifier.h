5403f9b2dfaffc030d03e5060e8d5340eccec8c1
jarikomppa <jari.komppa@gmail.com>
2016-05-20 13:35:54 +0300

    fix labels

--- src/noisemodifier.h
+++ src/noisemodifier.h
@@ -49,15 +49,15 @@ public:
 			ret = common();
 
 			if (ImGui::SliderFloat("##Strength", &mV, 0, 0.25f)) { gDirty = 1; }	ImGui::SameLine();
-			if (ImGui::Button("-#Strength") && mV - 0.001f >= 0) { gDirty = 1; mV -= 0.001f; } ImGui::SameLine();
-			if (ImGui::Button("+#Strength") && mV + 0.001f <= 0.25f) { gDirty = 1; mV += 0.001f; } ImGui::SameLine();
+			if (ImGui::Button("-##Strength") && mV - 0.001f >= 0) { gDirty = 1; mV -= 0.001f; } ImGui::SameLine();
+			if (ImGui::Button("+##Strength") && mV + 0.001f <= 0.25f) { gDirty = 1; mV += 0.001f; } ImGui::SameLine();
 			if (ImGui::Button("Reset##strength   ")) { gDirty = 1; mV = 0.1f; } ImGui::SameLine();
 			if (ImGui::Button("Randomize##strength")) { gDirty = 1; mV = 0.25f * ((SDL_GetTicks() * 74531) % 65535) / 65535.0f; } ImGui::SameLine();
 			ImGui::Text("Strength");
 
 			if (ImGui::SliderInt("##Seed    ", &mSeed, 0, 65535)) { gDirty = 1; } ImGui::SameLine();
-			if (ImGui::Button("-#Seed") && mSeed - 1 >= 0) { gDirty = 1; mSeed -= 1; } ImGui::SameLine();
-			if (ImGui::Button("+#Seed") && mSeed + 1 <= 65535) { gDirty = 1; mSeed += 1; } ImGui::SameLine();
+			if (ImGui::Button("-##Seed") && mSeed - 1 >= 0) { gDirty = 1; mSeed -= 1; } ImGui::SameLine();
+			if (ImGui::Button("+##Seed") && mSeed + 1 <= 65535) { gDirty = 1; mSeed += 1; } ImGui::SameLine();
 			if (ImGui::Button("Reset##seed       ")) { gDirty = 1; mSeed = 0; } ImGui::SameLine();
 			if (ImGui::Button("Randomize")) { gDirty = 1; mSeed = (SDL_GetTicks() * 74531) % 65535; } ImGui::SameLine();
 			ImGui::Text("Seed");
